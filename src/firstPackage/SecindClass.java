82eb43455f57f442474a75fd39436c425e103cab
jaiprakashkeswani <jaiprakash1091@gmail.com>
2021-05-22 17:33:46 +0530

    Added difference code

--- src/firstPackage/SecindClass.java
+++ src/firstPackage/SecindClass.java
@@ -5,6 +5,7 @@ public class SecindClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Friends: This is Second class");
+		System.out.println("Hello Friends: This is for difference");
 
 	}
 
